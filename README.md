9fe93730508cfd6fd6a0facb411a0b09c8c365eb
CoryKrol <16892390+CoryKrol@users.noreply.github.com>
2021-07-19 10:16:58 -0500

    Fix unit test package, add run instructions to README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+To run set the FLASK_APP environment variable
+```
+$ export FLASK_APP=hedgehog.py
+$ export FLASK_DEBUT=1
+$ flask run
+ * Serving Flask app "hedgehog.py" (lazy loading)
+ * Environment: production
+   WARNING: This is a development server. Do not use it in a production deployment.
+   Use a production WSGI server instead.
+ * Debug mode: on
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+
+```
--- hedgehog.py
+++ hedgehog.py
@@ -1,4 +1,6 @@
 import os
+import click
+
 from app import create_app, db
 from app.models import User, Role
 from flask_migrate import Migrate
@@ -13,12 +15,16 @@ def make_shell_context():
 
 
 @app.cli.command()
-def test():
+@click.argument('test_names', nargs=-1)
+def test(test_names):
     """
     Create a custom flask cli command to run the unit tests.
     Run with flask in terminal
     $ flask test
     """
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+        tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
