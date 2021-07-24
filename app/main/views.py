f5426ee8816ee583f7c076034e09c0185e08db37
CoryKrol <16892390+CoryKrol@users.noreply.github.com>
2021-07-24 18:36:47 -0500

    Add user profile code and templates

--- app/main/views.py
+++ app/main/views.py
@@ -1,18 +1,83 @@
 from datetime import datetime
 
-from flask import render_template, session, redirect, url_for
-
+from flask import abort, current_app, flash, render_template, session, redirect, request, url_for
+from flask_login import current_user, login_required
 from . import main
-from .forms import NameForm
+from .forms import EditProfileAdministratorForm, EditProfileForm, NameForm
+from .. import db
+from ..decorators import admin_required
+from ..models import Role, User
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        # ...
-        return redirect(url_for('.index'))
     return render_template('index.html',
-                           form=form, name=session.get('name'),
+                           name=session.get('name'),
                            known=session.get('known', False),
                            current_time=datetime.utcnow())
+
+
+@main.route('/edit-profile/<int:user_id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(user_id):
+    edit_user = User.query.get_or_404(user_id)
+    form = EditProfileAdministratorForm(user=edit_user)
+    if form.validate_on_submit():
+        edit_user.about_me = form.about_me.data
+        edit_user.confirmed = form.confirmed.data
+        edit_user.email = form.email.data
+        edit_user.location = form.location.data
+        edit_user.name = form.name.data
+        edit_user.role = Role.query.get(form.role.data)
+        edit_user.username = form.username.data
+        db.session.add(edit_user)
+        db.session.commit()
+        flash('Profile for ' + edit_user.username + ' updated successfully.')
+        return redirect(url_for('.user_profile', username=edit_user.username))
+    form.about_me.data = edit_user.about_me
+    form.confirmed.data = edit_user.confirmed
+    form.email.data = edit_user.email
+    form.location.data = edit_user.location
+    form.name.data = edit_user.name
+    form.role.data = edit_user.role_id
+    form.username.data = edit_user.username
+    return render_template('edit_profile.html', form=form, user=edit_user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        # Save submitted form
+        current_user.about_me = form.about_me.data
+        current_user.location = form.location.data
+        current_user.name = form.name.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash('Profile successfully updated.')
+        return redirect(url_for('.user_profile', username=current_user.username))
+    # Populate form with user data
+    form.about_me.data = current_user.about_me
+    form.location.data = current_user.location
+    form.name.data = current_user.name
+    return render_template('edit_profile.html', form=form)
+
+
+@main.route('/user/<username>')
+def user_profile(username):
+    user_object = User.query.filter_by(username=username).first_or_404()
+    return render_template('user_profile.html', user=user_object)
+
+
+@main.route('/shutdown')
+def server_shutdown():
+    """Used by unit tests to stop flask server"""
+    if not current_app.testing:
+        abort(404)
+    shutdown = request.environ.get('werkzeug.server.shutdown')
+    if not shutdown:
+        abort(500)
+    shutdown()
+    return 'Shutting down...'
