28fa1f7d02aac0589986eb3eb08b4226ef12e9ae
CoryKrol <16892390+CoryKrol@users.noreply.github.com>
2021-07-29 00:27:34 -0500

    Add requirements folder to split dev requirements from prod and add code to generate fake trade data

--- app/fake.py
+++ app/fake.py
@@ -1,20 +1,42 @@
+from random import randint, uniform
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.sql.expression import func, select
 from faker import Faker
 from . import db
-from .models import User
+from .models import User, Trade, Stock
 
 
 def users(count=100):
     fake = Faker()
     i = 0
     while i < count:
-        u = User(email=fake.email(),
-                 username=fake.user_name(),
-                 password='password',
-                 confirmed=True)
-        db.session.add(u)
+        user = User(email=fake.email(),
+                    username=fake.user_name(),
+                    password='password',
+                    confirmed=True,
+                    name=fake.name(),
+                    location=fake.city(),
+                    about_me=fake.text(),
+                    member_since=fake.past_date())
+        db.session.add(user)
         try:
             db.session.commit()
             i += 1
         except IntegrityError:
             db.session.rollback()
+
+
+def trades(count=100):
+    fake = Faker()
+    user_count = User.query.count()
+    stock_count = Stock.query.count()
+    for i in range(count):
+        user = User.query.offset(randint(0, user_count - 1)).first()
+        stock = Stock.query.offset(randint(0, stock_count - 1)).first()
+        trade = Trade(quantity=randint(0, 1000000),
+                      price=round(uniform(stock.year_low, stock.year_high), 2),
+                      timestamp=fake.past_date(),
+                      stock=stock,
+                      user=user)
+        db.session.add(trade)
+    db.session.commit()
